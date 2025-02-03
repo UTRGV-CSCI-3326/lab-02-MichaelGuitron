22f8e54b38a31233062224877cfecb60d572aa8f
MichaelGuitron <150745927+MichaelGuitron@users.noreply.github.com>
2025-02-03 04:52:22 +0000

    Commit

---
+++ src/MixMatch.java
@@ -0,0 +1,31 @@
+public class MixMatch {
+    public static void main(String[] args) {
+        String x = "Hello";
+        System.out.println(x + " <-String: is multiple characters." );
+        
+        char b = 'A';
+        System.out.println(b + " <-Char: is one singular letter");
+
+        short c = 32766;
+        System.out.println(c + "<- short: integers that range from -32,768 to 32,767.");
+
+        int d = 10;
+        System.out.println(d + " <- int: stores whole numbers no decimal numbers");
+
+        long e = 122220000;
+        System.out.println(e + "<-long: represents a 64-bit signed integer");
+
+        float f = 3.14159f;
+        System.out.println(f + " <-float: can hold decimal numbers ");
+
+        double g = 3.11111111111;
+        System.out.println(g +" <-double: can hold more intergers");
+        
+        boolean r = true;
+        System.out.println(r + " <- boolean: true or false");
+
+    }
+
+
+
+}
